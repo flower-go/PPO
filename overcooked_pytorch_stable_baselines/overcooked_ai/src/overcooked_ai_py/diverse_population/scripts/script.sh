a064ad13d7a23b7918504a33730077626c3c24b2
Petra Vysusilova <ayshi@alfrid.meta.zcu.cz>
2023-05-27 10:01:12 +0200

    funkcni run_uni, funkcni script.sh ale jeste nevim zda dobehne refpop trenovani

--- overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/script.sh
+++ overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/script.sh
@@ -3,13 +3,14 @@ module add conda-modules-py37
 conda activate "$home_dir"/envs/overcooked_ai_terminal
 
 cd $home_dir
-export CODEDIR=$(pwd)
+export CODEDIR=$(pwd)/coding
 echo "codedir: " $CODEDIR
 export PROJDIR="$home_dir"/coding/PPO/overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py
 echo "projdir: " $PROJDIR
 
 cd $PROJDIR
 pwd
+echo $home_dir
 echo "trained_models="$trained_models
 echo "exp"=$exp
 echo "eval_set_name"=$eval_set_name
