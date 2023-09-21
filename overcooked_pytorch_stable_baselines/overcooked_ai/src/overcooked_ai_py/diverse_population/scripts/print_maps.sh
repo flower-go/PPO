a01e46fc31d90cab0813ba2663bfc1f29eea9eb1
peta <doubravova.petramarie@gmail.com>
2023-09-21 16:12:34 +0200

    opraveno vypisovani do jobs_info

--- overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/print_maps.sh
+++ overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/print_maps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-home_dir="/storage/plzen1/home/ayshi"
 module add conda-modules-py37
 conda activate "$home_dir"/envs/overcooked_ai_terminal
 
@@ -8,11 +7,7 @@ export CODEDIR=$(pwd)/coding
 echo "codedir: " $CODEDIR
 export PROJDIR="$home_dir"/coding/PPO/overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py
 echo "projdir: " $PROJDIR
-cd $PROJDIR
-echo "jdu pustit python s parametry layout a inuput_file"
-echo $layout
-echo $input_file
-
+INFODIR="$home_dir"/coding/results
 echo -e "$PBS_JOBNAME\t$PBS_JOBID\t`hostname -f`\t$SCRATCHDIR" >> "$INFODIR"/jobs_info.txt
 
 #python diverse_population/visualisation/evaluation/eval_visualisation.py --layout_name=$layout --input_file=$input_file
