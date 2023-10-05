c2c7927365072419b0331884c62bfd1d492b4bf8
peta <doubravova.petramarie@gmail.com>
2023-10-05 15:32:22 +0200

    pokus o opravu data_transform

--- overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/visualisation/evaluation/data_transform.py
+++ overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/visualisation/evaluation/data_transform.py
@@ -1,49 +1,38 @@
 import ast
 
-file_name = "./diverse_population/visualisation/evaluation/test_file"
+#file_name = "./diverse_population/visualisation/evaluation/test_file"
 
 def load_data(filename):
     
-    print("filename")
-    print(filename)
+    print("filename {0}", filename)
     file1 = open(filename, 'r')
     terrain = None
     reward = None
     next_state = None
     result = {}
-    reading_log = True
     layout_name = None
 
     for line in file1:
         if line.startswith("layout_name"):
-            layout_name = line
+            layout_name = line[11:]
         if line.startswith("grid"):
             terrain = ast.literal_eval(line[5:])
-        if reading_log:
-            try:
-                if line.startswith("t:"):
-                    thread = str(line.split(":", 1)[1])
-                if line.startswith("r:"):
-                    reward = float(line.split(":", 1)[1])
-                    if result.get(thread) is None:
-                        result[thread] = [{"action": joint_action, "reward": reward, "next_state": next_state }]
-                    else:
-                        result[thread].append({"action": joint_action, "reward": reward, "next_state": next_state })
-                    continue
-                elif line.startswith("j:"):
-                    joint_action = ast.literal_eval(line.split(":", 1)[1])
-                elif line.startswith("n:"):
-                    next_state = ast.literal_eval(line.split(":", 1)[1])
-            except:
-                print("e")
-                print(line)
-                if line.startswith("r"):
-                    line = line[:-13]
+        try:
+            if line.startswith("t:"):
+                thread = str(line.split(":", 1)[1])
+            if line.startswith("r:"):
+                reward = float(line.split(":", 1)[1])
+                if result.get(thread) is None:
+                    result[thread] = [{"action": joint_action, "reward": reward, "next_state": next_state }]
                 else:
-                    try:
-                        line = (line.split(":", 1)[1]).rsplit("}",1)[0]
-                        print("nova line")
-                        print(line)
-                        next_state = ast.literal_eval(line + "}")
-                    except:
+                    result[thread].append({"action": joint_action, "reward": reward, "next_state": next_state })
+                continue
+            elif line.startswith("j:"):
+                joint_action = ast.literal_eval(line.split(":", 1)[1])
+            elif line.startswith("n:"):
+                next_state = ast.literal_eval(line.split(":", 1)[1])
+        except:
+            print("error")
+            print(line)
+
     return layout_name, terrain, result
