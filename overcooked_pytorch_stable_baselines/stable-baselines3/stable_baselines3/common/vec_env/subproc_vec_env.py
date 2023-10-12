e66d8c14b812fe65c17b48c70f4c0b767e9e3c27
peta <doubravova.petramarie@gmail.com>
2023-10-12 17:19:05 +0200

    furt nevim kterej step se vola

--- overcooked_pytorch_stable_baselines/stable-baselines3/stable_baselines3/common/vec_env/subproc_vec_env.py
+++ overcooked_pytorch_stable_baselines/stable-baselines3/stable_baselines3/common/vec_env/subproc_vec_env.py
@@ -45,7 +45,8 @@ def _worker(
         try:
             cmd, data = remote.recv()
             if cmd == "step":
-                observation, reward, done, info = env.step(data, logger = logger)
+                print(env.step)
+                observation, reward, done, info = env.step(data)
                 if done:
                     # save final observation where user can get it, then reset
                     info["terminal_observation"] = observation
