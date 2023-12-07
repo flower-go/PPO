057048b366e2225b30fe07fc75d7899b640ecc5d
Petra Vysusilova <ayshi@alfrid.metacentrum.cz>
2023-12-07 17:28:46 +0100

    funkcni trenovani base populace a evaluace, funkcn kontrola ze mam mapy a davani map do pdf

--- overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/check_basic_pop.py
+++ overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/check_basic_pop.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import datetime
 codedir = os.environ["CODEDIR"]
 #codedir = /home/premek/DP/
 projdir = os.environ["PROJDIR"]
@@ -16,7 +17,7 @@ maps = [
     "schelling",
     "centre_pots",
     # "corridor",
-    "pipeline",
+    # "pipeline",
     "scenario1_s",
     "large_room",
     "asymmetric_advantages",  # tady
@@ -48,7 +49,7 @@ def check_models(name_1, name_2 = None, count = 30):
         try:
             files = [f.path for f in os.scandir(path) if f.is_file()]
         except:
-            print(f"Path  {path} probably not found")
+            print(f"Path to model  {path} probably not found")
         results[map] = {}
         num_models = len(files)
         results[map]["model_count"] = num_models
@@ -62,18 +63,43 @@ def check_maps(name_1, name_2 = None):
     is_ok = True
     for map in maps:
         name = name_1 + map + name_2
-        path = "./diverse_population/visualisation/" + map + "/" + name + "/"
+        path = "./diverse_population/visualisation/" + map + "/" 
         print(f"searching models in: {path}")
         try:
-            files = [f.path for f in os.scandir(path) if f.is_file()]
+            files = [f.path for f in os.scandir(path) if f.is_file() and f.name.startswith(name_1)]
             results[map] = {}
             results[map]["map_file"] = [(f, os.path.getmtime(f)) for f in files][0]
         except Exception as error:
-            print(f"Path  {path} probably not found. Error {error}")
+            print(f"Visual not found  {path} probably not found. Error {error}")
     return results, is_ok
 
+import shutil
+def copy_maps(filenames, dest_dir):
+    for f in filenames:
+        shutil.copy2(f, dest_dir) 
 
 if __name__ == "__main__":
-    # res, ok = check_models(name_1 = "nost1_", name_2 = "_ref_30")
-    res, ok = check_maps(name_1 = "nost1_", name_2 = "_ref_30")
-    print(res)
+    res, ok = check_models(name_1 = "nost1_", name_2 = "_ref_30")
+    res_m, ok = check_maps(name_1 = "nost1_", name_2 = "_ref_30")
+#    print(res)
+    no_map = []
+    yes_map = []
+    print("*************************************")
+    print("Visualisation matrices are for maps:")
+    for map in maps:
+        map_file = res_m[map].get("map_file")
+
+        if map_file is not None:
+            print(f"{map}")
+            yes_map.append(map_file[0])
+        else:
+            no_map.append(map)
+    print("*************************************")
+    print("no visualisation file for:")
+    print(*no_map, sep="\n")
+ #   print("yes maps")
+#    print(yes_map)
+    dir_name = projdir + "/diverse_population/visualisation/matrices" + f"{datetime.datetime.now():%Y_%m_%d_%H_%M_%S}"
+    print(dir_name)
+    os.makedirs(dir_name)
+    copy_maps(yes_map,dir_name)
--- overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/visualisation/maps/maps_to_pdf.py
+++ overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/visualisation/maps/maps_to_pdf.py
@@ -11,7 +11,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str, help="Path to the dictionary with png files")
 parser.add_argument("--filename", type=str, help="Name of the resulting pdf file")
 parser.add_argument("--title", type=str, help="Header of the pdf")
-parser.add_argument("--columns", deafult=4, type=int, help="number of columns")
+parser.add_argument("--columns", default=4, type=int, help="number of columns")
+parser.add_argument("--prefix", default="", type=str, help="prefix ot be removed")
+parser.add_argument("--postfix", default="", type=str, help="postfix to be removed")
 args = parser.parse_args([] if "__file__" not in globals() else None)
 
 import os
@@ -22,50 +24,55 @@ path = '.'
 filename = 'maps_0604.pdf'
 title = 'Test'
 
+desired_width = 100
+desired_height = 100
+TEXT_SIZE = 8
+Y_POS_START = A4[1] - 1.1*desired_width
 
-Y_POS_START = 250
-
-
-def createPDF(path_to_images, document_name, document_title):
+def createPDF(path_to_images, document_name, document_title, columns, args):
     th_size = 180*4/columns
-    img_size = 115*4/columns
-    x_space = th_size / (columns + 4)
+    img_size = desired_width
+    x_space = 8
     X_POS_START = x_space * 2
-    x_increment = img_size + x_space
+    x_increment = desired_width + x_space
     rows_in_page = mt.ceil((A4[1]/mm)/(th_size/mm))
     print(rows_in_page)
-    print("x increment {0}, th_size {1}, img_size {2}".format(x_increment,th_size,img_size))
+    print("x increment {0}, th_size {1}, img_size {2}".format(x_increment,th_size,desired_width))
 
     def rowGen(list_of_images):  # Creates a list of 4 image rows
-
+        print(f"I have {len(list_of_images)} images")
         for i in range(0, len(list_of_images), columns):
             yield list_of_images[i:i + columns]
 
     def renderRow(path_to_images, row, y_pos):  # Renders each row to the page
 
         x_pos = X_POS_START  # starting x position
-        thumb_size = th_size, th_size  # Thumbnail image size
 
         for i in row:
             image_filename = i.split(".")[0]  # Saves the filename for use in the draw string operation below
             img = Image.open(os.path.join(path_to_images, i))  # Opens image as a PIL object
-            img.thumbnail(thumb_size)  # Creates thumbnail
-
+            width, height= img.size
             img = ImageReader(img)  # Passes PIL object to the Reportlab ImageReader
-
+            start = int(len(args.prefix))
+            print(f"image size is {width},{height}")
+            end = -1*int(len(args.postfix))
+            print(f"image name is {image_filename} length is {len(image_filename)} and indices are {start} and {end}")
+            image_filename = image_filename[start : end]
             # Lays out the image and filename
-            pdf.drawImage(img, x_pos , y_pos * mm, width=img_size, height=img_size, preserveAspectRatio=True, anchor='c')
-            pdf.drawCentredString((x_pos + img_size/2), (y_pos + (img_size + 5)/mm) * mm, image_filename)
-
+            img_ratio = width/desired_width
+            pdf.drawImage(img,x_pos,y_pos, width = width/img_ratio, height = height/img_ratio)
+            #pdf.drawImage(img, x_pos , y_pos * mm, preserveAspectRatio=True, anchor='c')
+            print(image_filename)
+            pdf.drawCentredString((x_pos + img_size/2), (y_pos - 0*img_size - TEXT_SIZE), image_filename)
             x_pos += x_increment  # Increments the x position ready for the next image
-
     images = [i for i in os.listdir(path_to_images) if
               i.endswith('.png')]  # Creates list of images filtering out non .jpgs
     row_layout = list(rowGen(images))  # Creates the layout of image rows
 
     pdf = canvas.Canvas(document_name, pagesize=A4, pageCompression=1)
     pdf.setTitle(document_title)
-
+    pdf.drawCentredString(A4[0] / 2.0, Y_POS_START - TEXT_SIZE - 8+ desired_width*1.1,document_title) # or: pdf_text_object.textLine(text) etc.
+    #print(f"Title is {document_title}")
     rows_rendered = 0
 
     y_pos = Y_POS_START # Sets starting y pos
@@ -76,17 +83,18 @@ def createPDF(path_to_images, document_name, document_title):
         if rows_rendered == rows_in_page:
 
             pdf.showPage()
-            pdf.setFont('Helvetica', 10)
-            y_pos = 250
+            pdf.setFont('Helvetica', TEXT_SIZE)
+            y_pos = Y_POS_START
             rows_rendered = 0
 
         else:
 
             renderRow(path_to_images, row, y_pos)
-            y_pos -= 40
+            y_pos -= desired_width
             rows_rendered += 1
-
+ #       break    
     pdf.save()
 if __name__ == "__main__":
     #createPDF(path, filename, title, 5)
-    createPDF(args.path, args.filename, args.title, args.columns)
+    print(f"path to images: {args.path}")
+    createPDF(args.path, args.filename, args.title, args.columns,args)
