0f4cc363dd44b53b24be8a83a39059663de2ebb3
Petra Vysušilová <petra.vysusilova@skippay.cz>
2024-04-29 11:06:21 +0200

    html rendering in progress

---
+++ overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/scripts/html_rendering/html_results.py
@@ -0,0 +1,85 @@
+#import jinja2
+#environment = jinja2.Environment()
+
+#template = environment.from_string("Hello, {{ name }}!")
+#a = template.render(name="World")
+#print(a)
+
+# write_messages.py
+CODE_PATH = "C:/Users/PetraVysušilová/Documents/coding/PPO/overcooked_pytorch_stable_baselines/overcooked_ai/src/overcooked_ai_py/diverse_population/"
+heat_path = "visualisation/"
+
+from jinja2 import Environment, FileSystemLoader
+
+
+max_score = 100
+test_name = "Python Challenge"
+layouts_onions = [
+           "small_corridor",
+           "five_by_five",
+           "schelling",
+           "centre_pots",
+           "corridor",
+           "pipeline",
+           "scenario1_s",
+           "large_room",
+           "asymmetric_advantages", #tady
+           "schelling_s",
+            "coordination_ring", #tady
+           "counter_circuit_o_1order", #tady
+           "long_cook_time",
+           "cramped_room", #tady
+           "forced_coordination", #tady
+           "m_shaped_s",
+           "unident",
+           "simple_o",
+           "centre_objects",
+           "scenario2_s",
+           "scenario3",
+           "scenario2",
+           "scenario4",
+           "bottleneck",
+           "tutorial_0"]
+
+frame_stacking = {"chan":("channels",4),
+                  "tupl":("tuple",4),
+                  "nost":("channels",1) #effectively no stacking
+                  }
+
+environment = Environment(loader=FileSystemLoader("C:\\Users\\PetraVysušilová\\Documents\\coding\\PPO\\overcooked_pytorch_stable_baselines\\overcooked_ai\\src\\overcooked_ai_py\\diverse_population\\scripts\\html_rendering\\templates"))
+template = environment.get_template("results.txt")
+res_dict = {}
+
+for i in layouts_onions:
+    res_dict[i] = {}
+    for s in frame_stacking:
+        res_dict[i][s] = {}
+
+def heat_maps():
+    result = []
+    path = CODE_PATH + heat_path
+    for map in layouts_onions:
+        for s in frame_stacking:
+            file = f"{path}/{map}/{s}_{map}_ref-30_reordered.png"
+            res_dict[map][s]["heat"] = file
+            #result.append(file)
+
+def heat_steps():
+    path = CODE_PATH + heat_path
+    for map in layouts_onions:
+        for s in frame_stacking:
+            file = f"{path}/{map}/steps2754060_{s}_{map}_ref-30_reordered.png"
+            res_dict[map][s]["heat_s2"] = file
+            file = f"{path}/{map}/steps1377030_{s}_{map}_ref-30_reordered.png"
+            res_dict[map][s]["heat_s1"] = file
+
+
+
+heat_maps()
+heat_steps()
+with open("results.html", mode="w", encoding="utf-8") as results:
+    results.write(template.render(maps=layouts_onions, res = res_dict))
+    print(f"... wrote {results}")
+
+
+
